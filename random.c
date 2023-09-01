628efb9a66eae25e6295097a93f6afbfe2d1f1ef
eliasghiz <tum98557@cis-lclient01.cis.temple.edu>
2023-09-01 09:17:04 -0400

    Added random.c to git repo

---
+++ random.c
@@ -0,0 +1,26 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+int randchar();
+int main();
+
+int randchar(){
+
+    srand ( time(NULL) );
+
+
+    int i;
+
+    for (i = 0;i<7;i++) {
+        char rletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" [rand() % 26];
+        printf("%c", rletter);
+    }
+
+    return 0;
+}
+int main(){
+    randchar();
+    return 1;
+}
+
